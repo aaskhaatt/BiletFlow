b2fc23104cb1269047b8ee538b03231a82715f7e
Askhat <askat.amandykov@nu.edu.kz>
2026-09-09 19:37:13 +0500

    Set up project database and Alembic

---
+++ database.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+import os
+
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+
+SessionLocal = sessionmaker(
+    bind=engine, 
+    autoflush=False,
+    autocommit=False
+)
+
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
